c9628687fb8757fbeb31f21d8f2dfbfcb12631b2
Andres Gonzalez <andresg0896@gmail.com>
2024-09-17 02:25:43 +0000

    edit sources with identifiers and names

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,4 +1,4 @@
 select 
     id as customer_id,
     first_name,
-    last_name from {{ source('dbt_testing', 'jaffle_shop_customers') }}
+    last_name from {{ source('jaffle_shop', 'customers') }}
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,4 +3,4 @@ select
     orderid as order_id,
     amount/100 as amount,
     status
-    from {{ source('dbt_testing', 'stripe_payment') }}
+    from {{ source('stripe', 'payment') }}
