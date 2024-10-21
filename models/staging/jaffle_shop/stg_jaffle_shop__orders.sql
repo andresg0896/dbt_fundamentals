c9d90ea10576e5ce20097f9131852f748f838f11
Andres Gonzalez <andresg0896@gmail.com>
2024-10-21 16:57:36 +0000

    moving staging models

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,25 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-    from {{ source('jaffle_shop', 'orders') }}
+with
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+transformed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        row_number() over (
+        partition by user_id 
+        order by order_date, id
+        ) as user_order_seq,
+        status as order_status
+
+    from source
+
+)
+
+select * from transformed
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,6 +1,22 @@
-select 
-    id as payment_id,
-    orderid as order_id,
-    amount/100 as amount,
-    status
-    from {{ source('stripe', 'payment') }}
+with
+
+source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+transformed as (
+
+    select 
+
+        id as payment_id,
+        orderid as order_id,
+        round(amount/100, 2) as payment_amount,
+        status as payment_status
+
+    from source
+
+)
+
+select * from transformed
