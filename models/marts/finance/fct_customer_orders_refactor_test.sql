6cac81a625e634bc964acaef9087c95527bec9bc
Andres Gonzalez <andresg0896@gmail.com>
2024-10-25 02:51:44 +0000

    refactoring course finished

--- models/marts/finance/fct_customer_orders_refactor_test.sql
+++ models/marts/finance/fct_customer_orders_refactor_test.sql
@@ -62,17 +62,17 @@ customer_orders as (
 final as(
 
     select
-        p.*,
-        row_number() over (order by p.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-        case when c.first_order_date = p.order_placed_at
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+        case when customer_orders.first_order_date = paid_orders.order_placed_at
         then 'new'
         else 'return' end as nvsr,
         unknown_paid.clv_bad as customer_lifetime_value,
-        c.first_order_date as fdos
-    from paid_orders p
-    left join customer_orders as c using (customer_id)
-    left outer join unknown_paid on unknown_paid.order_id = p.order_id
+        customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
+    left outer join unknown_paid on unknown_paid.order_id = paid_orders.order_id
     order by order_id
 )
 
